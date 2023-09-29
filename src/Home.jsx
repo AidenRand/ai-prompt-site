c1a44c743b7000114dc937bb4a8a1b0df59b8f23
Cortrax <aidenrphotos@gmail.com>
2023-09-29 12:24:02 -0400

    Add reprompting

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,8 +9,14 @@ async function makeResponse(user_input_array) {
         dangerouslyAllowBrowser: true,
     });
 
+    console.log('user_input_array', user_input_array);
+    // const messages = user_input_array.map(input => {
+    //     return {
+
+    //     }
+    // })
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: user_input_array }],
+        messages: user_input_array,
         model: 'gpt-4',
     });
 
@@ -26,17 +32,21 @@ function Home() {
 
     const handleInput = () => {
         const user_input = inputRef.current.value;
-        setInputs([...inputs, user_input]);
-        setConversation([...conversation, { input: user_input, output: '' }]);
+        // setInputs([...inputs, user_input]);
+        // setConversation([
+        //     ...conversation,
+        //     { role: 'user', content: user_input },
+        // ]);
+        const convArr = [];
+        convArr.push(...conversation, { role: 'user', content: user_input });
+        console.log('my conversation', conversation);
         const handleResponse = async () => {
             try {
-                const response = await makeResponse(conversation);
+                // console.log('is this empty?', conversation);
+                const response = await makeResponse(convArr);
                 setConversation([
-                    ...conversation,
-                    {
-                        input: user_input,
-                        output: response,
-                    },
+                    ...convArr,
+                    { role: 'assistant', content: response },
                 ]);
             } catch (err) {
                 console.log('error', err);
@@ -52,8 +62,8 @@ function Home() {
             <div className='prompt-container'>
                 {conversation.map((messages, index) => (
                     <div className='input-div' key={index}>
-                        <p className='user-input-text'>{messages.input}</p>
-                        <p>{messages.output}</p>
+                        <p className='user-input-text'>{messages.role}</p>
+                        <p>{messages.content}</p>
                     </div>
                 ))}
             </div>
